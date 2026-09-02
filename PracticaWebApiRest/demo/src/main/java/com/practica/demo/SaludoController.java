9d60dc6f1c8aaefe27d77935806a9a35d6c44fcf
Jefferson Veloz <jeffersonvelozh@gmail.com>
2026-09-02 11:00:32 -0500

    probando el apiresrt de java con srping

---
+++ PracticaWebApiRest/demo/src/main/java/com/practica/demo/SaludoController.java
@@ -0,0 +1,15 @@
+package com.practica.demo;
+
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.RequestParam; 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String saludar(@RequestParam(defaultValue = "Mundo") String nombre) {
+        return "¡Hola, " + nombre + "!";
+    }
+
+}
--- PracticaWebApiRest/demo/src/test/java/com/practica/demo/DemoApplicationTests.java
+++ PracticaWebApiRest/demo/src/test/java/com/practica/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.integrity.demo;
+package com.practica.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
