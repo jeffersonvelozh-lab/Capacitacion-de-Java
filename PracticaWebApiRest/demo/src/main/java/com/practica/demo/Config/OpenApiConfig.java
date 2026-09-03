2b86afb158529d5a9095658232efbddd3e4fcd37
Jefferson Veloz <jeffersonvelozh@gmail.com>
2026-09-03 15:12:03 -0500

    Agregando documentacion en la practica

---
+++ PracticaWebApiRest/demo/src/main/java/com/practica/demo/Config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package com.practica.demo.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "API de Pokemones",
+        version = "1.0",
+        description = "CRUD de practicas para capacitacion de Java"
+    )
+)
+public class OpenApiConfig {
+    
+}
